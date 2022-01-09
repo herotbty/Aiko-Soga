4be89e9fd630650c0dd5d468dbfcb9d0c48e0126
Aiko <herotbty@gmail.com>
2022-01-09 18:17:48 +0700

    Update soga.sh

--- soga.sh
+++ soga.sh
@@ -235,7 +235,7 @@ show_log() {
 }
 
 install_bbr() {
-    bash <(curl -L -s https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh)
+    bash <(curl -L -s https://raw.githubusercontent.com/herotbty/Aiko-bbr-Crack/master/tcp.sh)
     if [[ $? == 0 ]]; then
         echo ""
         echo -e "${green}Quá trình cài đặt bbr thành công, vui lòng khởi động lại máy chủ${plain}"
